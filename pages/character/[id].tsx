4169ce3b2ff971cecbab499c53aa380c02c14941
gor-dey <gordeychepikov@gmail.com>
2024-10-05 11:06:12 +0500

    fix

--- pages/character/[id].tsx
+++ pages/character/[id].tsx
@@ -34,21 +34,21 @@ const GET_CHARACTER_DETAILS = gql`
 `;
 
 export default function CharacterDetails({ character }: { character: Person }) {
+  const characterAttributes = [
+    { label: "Birth Year", value: character.birthYear },
+    { label: "Gender", value: character.gender },
+    { label: "Height", value: `${character.height} cm` },
+    { label: "Mass", value: `${character.mass} kg` },
+  ];
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">{character.name}</h1>
-      <p>
-        <strong>Birth Year:</strong> {character.birthYear}
-      </p>
-      <p>
-        <strong>Gender:</strong> {character.gender}
-      </p>
-      <p>
-        <strong>Height:</strong> {character.height} cm
-      </p>
-      <p>
-        <strong>Mass:</strong> {character.mass} kg
-      </p>
+      {characterAttributes.map((attr) => (
+        <p key={attr.label}>
+          <strong>{attr.label}:</strong> {attr.value}
+        </p>
+      ))}
       <h2 className="text-xl font-semibold mt-4">Films:</h2>
       <ul>
         {character.filmConnection?.films.map((film: Film, index: number) => (
